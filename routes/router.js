5fef9d8c2d7ab0dbcc2179d92b790386a0ef0c9c
MuraliRB <muralirb1999@gmail.com>
2023-06-25 22:43:47 +0530

    crud completed

--- routes/router.js
+++ routes/router.js
@@ -7,6 +7,8 @@ router.get("/", (req, res) => {
   console.log("Connect");
 });
 
+//Insert User
+
 router.post("/api/register", async (req, res) => {
   console.log(req.body);
   const { name, age, email, password } = req.body;
@@ -32,7 +34,7 @@ router.post("/api/register", async (req, res) => {
   }
 });
 
-//getuser
+//getAllUser
 
 router.get("/api/getdata", async (req, res) => {
   try {
@@ -40,6 +42,47 @@ router.get("/api/getdata", async (req, res) => {
     res.status(201).json(getUser);
   } catch (error) {
     res.status(404).json(error);
+  }
+});
+
+//getIndividualUser
+
+router.get("/api/getuser/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const individualUser = await users.findById({ _id: id });
+    res.status(201).json(individualUser);
+  } catch (error) {
+    res.status(404).json(error);
+  }
+});
+
+//updateUser
+
+router.patch("/api/update/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const updateUser = await users.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.status(201).json(updateUser);
+  } catch (error) {
+    res.status(422).json(error);
+  }
+});
+
+//deleteUser
+
+router.delete("/api/delete/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const deleteUser = await users.findByIdAndDelete({ _id: id });
+    res.status(201).json(deleteUser);
+  } catch (error) {
+    res.status(422).json(error);
   }
 });
 
